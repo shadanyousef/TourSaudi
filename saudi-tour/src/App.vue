40c0ddaf3d64c438c189b2edd9c7e0481a838fda
shadanyousef <shadanyousef19@hotmail.com>
2024-10-29 23:50:05 +0300

    vue setup

---
+++ saudi-tour/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+ 
+</script>
+
+<template>
+ 
+
+    <div class="wrapper">
+   <h1>TourSaudi</h1>
+    </div>
+   
+
+  
+</template>
+
+<style scoped>
+ 
+</style>
